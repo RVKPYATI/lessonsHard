98e9fc0060f887ce566055ced1702a9bd6b06125
Vasiliy Rusanov <rvk1986@mail.ru>
2022-10-03 16:13:18 +0300

    Переделал усложненое задание 3 урока

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 let lang = 'ru';
+const daysRu = "'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'";
+const daysEng = "'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'";
 
 if (lang === 'ru') {
-    console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+    console.log(daysRu);
 } else if (lang === 'eng') {
-    console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+    console.log(daysEng);
 }
 
 switch (lang) {
     case 'ru':
-        console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+        console.log(daysRu);
         break;
     case 'eng':
-        console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+        console.log(daysEng);
         break;
 }
 
-let lang2 = [['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']];
-console.log(lang2[0]);
+let lang2 = new Map ([['ru', "'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'"],['eng', "'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'"]]);
+console.log(lang2.get('ru'));
 
 let namePerson = prompt('Введите имя');
 let result = (namePerson =='Артем') ? console.log('Директор'): (namePerson =='Александр') ? console.log('Преподаватель') : console.log('Студент');
