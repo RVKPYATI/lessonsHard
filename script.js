0f51dded31a4d6be9bc7847630a3ccd0c0bc0e28
Vasiliy Rusanov <rvk1986@mail.ru>
2022-10-03 10:21:40 +0300

    complette lesson 3 hard

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-let num = 266219;
+let lang = 'ru';
 
-
-let numString = num.toString();
-let result = 1;
-for (let i=0; i < numString.length; i++) {
-    
-    result *= numString[i];
+if (lang === 'ru') {
+    console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+} else if (lang === 'eng') {
+    console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
 }
 
+switch (lang) {
+    case 'ru':
+        console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+        break;
+    case 'eng':
+        console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+        break;
+}
 
-result **= 3;
+let lang2 = [['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']];
+console.log(lang2[0]);
 
-console.log(result.toString().slice(0,2));
+let namePerson = prompt('Введите имя');
+let result = (namePerson =='Артем') ? console.log('Директор'): (namePerson =='Александр') ? console.log('Преподаватель') : console.log('Студент');
