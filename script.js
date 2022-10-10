85ac6d1dbe10d335b4eebb9ee0e52921d13dc54b
Vasiliy Rusanov <rvk1986@mail.ru>
2022-10-10 08:11:10 +0300

    Complette mission 1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+'use strict';
+
+const arr = ['56', '78', '123', '456', '99', '744', '204'];
+
+
+for (let i = 0; i < arr.length; i++) {
+    
+    if(arr[i].startsWith('2') || arr[i].startsWith('4')) {
+        console.log(arr[i]);
+    }
+}
 
