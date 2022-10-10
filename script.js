4aecdd00d409c7a276094084f6f639d7259f5d0c
Vasiliy Rusanov <rvk1986@mail.ru>
2022-10-10 07:52:58 +0300

    Clear file

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
 
-function getStr(string) {
-    if (typeof string !== 'string') {
-        console.log('Вы ввели не строку');
-        return null;
-    } 
-        string = string.trim();
-        if(string.length > 30) {
-            string = string.substr(0, 30) + '...';
-        }
-    
-    return string;
-}
-
-console.log(getStr('  Привет как дела что делаешь в этом бренном мире'));
