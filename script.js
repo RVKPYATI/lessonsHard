05a4da49e4cb654e8ab88654dec8fb1e3f3f7cb6
Vasiliy Rusanov <rvk1986@mail.ru>
2022-11-03 06:34:20 +0300

    Начало усложненого задания 7 урока

--- script.js
+++ script.js
@@ -1,26 +1,2 @@
 'use strict';
 
-const arr = ['56', '78', '123', '456', '99', '744', '204'];
-
-function isPrime(num) {
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            return false;
-        }
-    }
-
-    return num > 1;
-}
-
-for (let i = 0; i < arr.length; i++) {
-    
-    if(arr[i].startsWith('2') || arr[i].startsWith('4')) {
-        console.log(arr[i]);
-    }
-}
-
-for (let i = 2; i <= 100; i++) {
-    if(isPrime(i)) {
-        console.log(i, `Делители этого числа 1 и ${i}`);
-    }
-}
