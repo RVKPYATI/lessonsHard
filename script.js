5ef8e8198f51e70cac7b4c2ba7dc59f3ce6fc56e
Vasiliy Rusanov <rvk1986@mail.ru>
2022-11-03 07:15:45 +0300

    Выполнил задание

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота','Воскресенье'];
+const div = document.querySelector('div');
+const dataTime = new Date();
+
+
+week.forEach((item,index)=>{
+    
+    if(index === dataTime.getDay()-1) {
+        div.innerHTML += `<b>${item}</b><br>`;
+    } else if(index === 5 || index === 6) {
+        div.innerHTML += `<i>${item}</i><br>`;
+    } else {
+        div.innerHTML += item + '<br>';
+    }
+});
+
+
